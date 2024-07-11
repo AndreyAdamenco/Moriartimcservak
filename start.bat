fdae15e124e5f6f57906959888c28b5f4f1da2ca
Sans228 <93283608+AndreyAdamenco@users.noreply.github.com>
2024-07-11 21:10:30 +0000

    Update 1.0

--- start.bat
+++ start.bat
@@ -1,7 +1,7 @@
 @echo off
 
 :start
-java -Xmx4096M -Xms4096M -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15 -jar paper-1.20.4-485.jar 
+java -Xmx7G -Xms1G -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:MaxInlineLevel=15 -jar paper-1.20.4-485.jar 
 
 echo Server restarting...
 echo Press CTRL + C to stop.
